ca07f7b77b23dec468ffe4ca415ccdb7c31fc7aa
“ravi <ravikumarmohane@gmail.com>
2023-06-01 08:49:50 +0530

    [RAVI KUMAR]:-prime number

---
+++ prime number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class PrimeNumberChecker
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isPrime = true;
+
+        if (number < 2)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine($"{number} is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is not a prime number.");
+        }
+    }
+}
