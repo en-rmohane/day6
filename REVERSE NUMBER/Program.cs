cba385f50dcda902ac969acb84022c58a07b8328
“ravi <ravikumarmohane@gmail.com>
2023-06-01 08:46:01 +0530

    [ravi kumar]:-reverse number

---
+++ REVERSE NUMBER/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class ReverseNumberProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversedNumber = ReverseNumber(number);
+
+        Console.WriteLine($"Reversed number: {reversedNumber}");
+    }
+
+    static int ReverseNumber(int number)
+    {
+        int reversedNumber = 0;
+
+        while (number != 0)
+        {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return reversedNumber;
+    }
+}
