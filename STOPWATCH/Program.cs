f0d24288408ad514614e71af9a4f4fb05b17587e
“ravi <ravikumarmohane@gmail.com>
2023-06-01 08:39:22 +0530

    [RAVI KUMAR]:-STPOWATCH

---
+++ STOPWATCH/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+class StopwatchProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("Press any key to start the stopwatch...");
+        Console.ReadKey();
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        Console.WriteLine("Stopwatch started. Press any key to stop...");
+        Console.ReadKey();
+
+        stopwatch.Stop();
+
+        TimeSpan elapsedTime = stopwatch.Elapsed;
+        Console.WriteLine($"Elapsed Time: {elapsedTime}");
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
