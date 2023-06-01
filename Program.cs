e344b523b4640fd769c081b0561d3f585a177da5
mohane7247 <131001045+mohane7247@users.noreply.github.com>
2023-06-01 08:53:32 +0530

    perfect number

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class FibonacciSeries
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of terms you want in the Fibonacci series:");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci series:");
+        PrintFibonacciSeries(count);
+    }
+
+    static void PrintFibonacciSeries(int count)
+    {
+        int first = 0, second = 1;
+
+        if (count >= 1)
+        {
+            Console.Write(first + " ");
+        }
+        if (count >= 2)
+        {
+            Console.Write(second + " ");
+        }
+
+        for (int i = 3; i <= count; i++)
+        {
+            int next = first + second;
+            Console.Write(next + " ");
+
+            first = second;
+            second = next;
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ perfectnumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class perfectnumber
+    {
+        
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (IsPerfectNumber(number))
+            {
+                Console.WriteLine(number + " is a perfect number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a perfect number.");
+            }
+        }
+
+        static bool IsPerfectNumber(int number)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum == number;
+        }
+    }
+
+}
+
